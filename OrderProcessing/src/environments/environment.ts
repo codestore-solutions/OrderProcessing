520b1a792e763decc85f59100f1c08802dfa6746
siddharth-garg-cs <siddharth.g@codestoresolutions.com>
2023-08-22 19:26:04 +0530

    Revert "changed environment file and services"
    This reverts commit 9cbb874293672bd53f966ce795e1fb5f7936293a.

--- OrderProcessing/src/environments/environment.ts
+++ OrderProcessing/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
+    orderURL : "https://order-processing-dev.azurewebsites.net/api/v1/vendor/",
+    order: "https://order-processing-dev.azurewebsites.net/api/v1/order/",
     orderProcessingUrl: "https://order-processing-dev.azurewebsites.net/api/v1/"
 }
