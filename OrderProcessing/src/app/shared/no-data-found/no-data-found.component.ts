fea04eb39e7beb739b02b9d5b8645a412802d498
siddharth-garg-cs <siddharth.g@codestoresolutions.com>
2023-08-28 14:00:21 +0530

    configurable data handling component

--- OrderProcessing/src/app/shared/no-data-found/no-data-found.component.ts
+++ OrderProcessing/src/app/shared/no-data-found/no-data-found.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-no-data-found',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./no-data-found.component.scss']
 })
 export class NoDataFoundComponent {
+  @Input() dataMissingMessage!:string;
 
 }
