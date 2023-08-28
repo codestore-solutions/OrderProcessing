907c3e4a62e58599f2fe2034a72313b35d2c28e5
siddharth-garg-cs <siddharth.g@codestoresolutions.com>
2023-08-28 11:21:35 +0530

    added configurable custom status timeline bar

--- OrderProcessing/src/app/navigation/order-timeline-bar/order-timeline-bar.component.ts
+++ OrderProcessing/src/app/navigation/order-timeline-bar/order-timeline-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-order-timeline-bar',
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-timeline-bar.component.scss']
 })
 export class OrderTimelineBarComponent {
+  
+  @Input() currentOrderStatus!: number;
 
+  orderStatuses: {statusCode:number; statusName:string;}[] = [
+    {
+      statusCode: 1,
+      statusName:"New"
+    },
+    {
+      statusCode: 4,
+      statusName:"Packing Completed"
+    },
+    {
+      statusCode: 8,
+      statusName:"Picked Up"
+    },
+    {
+      statusCode: 10,
+      statusName:"Not Accepted by Customer"
+    },
+    {
+      statusCode: 11,
+      statusName:"Delivered"
+    },
+    {
+      statusCode: 12,
+      statusName:"Return"
+    },
+    {
+      statusCode: 13,
+      statusName:"Exchanged"
+    },
+    {
+      statusCode: 14,
+      statusName:"Payment Failed"
+    },
+    {
+      statusCode: 16,
+      statusName:"Cancelled by Customer"
+    }
+  ];
+  
 }
