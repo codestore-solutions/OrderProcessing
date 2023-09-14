f7e4ecf85a5918350181edf8f02a604b5a956d6e
siddharth-garg-cs <siddharth.g@codestoresolutions.com>
2023-09-14 15:50:00 +0530

    fix: sidenav for smaller devices

--- OrderProcessing/src/app/navigation/dashboard/dashboard.component.ts
+++ OrderProcessing/src/app/navigation/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
+import { Observable, map, share } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  isHandset$: Observable<boolean> = this._breakPoint.observe('(max-width: 850px)').pipe(
+    map(result=>result.matches),
+    share()
+  );
 
   constructor(
-    private _router: Router
+    private _router: Router,
+    private _breakPoint: BreakpointObserver
   ){}
 
   logout():void{
